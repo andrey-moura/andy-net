7e4eda1802c52188e72cece8763cb954e38bf606
Andrey Moura <andreymds88@gmail.com>
2023-07-01 18:49:03 -0300

    Only add web app if files are provided, not try to build jobs if juice is not available

--- networking.cmake
+++ networking.cmake
@@ -18,10 +18,6 @@ function(add_web_app name)
         "${CMAKE_CURRENT_LIST_DIR}/src/jobs/*.cpp"
     )
 
-    file(GLOB_RECURSE jobs_files CONFIGURE_DEPENDS 
-        "${CMAKE_CURRENT_LIST_DIR}/src/jobs/*.cpp"
-    )
-
     SET(source_files
         ${source_files}
         "${CMAKE_CURRENT_LIST_DIR}/config/routes.cpp")
@@ -31,26 +27,44 @@ function(add_web_app name)
     include_directories("${CMAKE_CURRENT_LIST_DIR}/include/models")
     include_directories("${CMAKE_CURRENT_LIST_DIR}/include/jobs")
     
-    add_executable(
-        # The name of your game
-        ${name}
-        
-        ${source_files}
-    )
+    if(NOT source_files)
+        message(STATUS "The web app ${name} won't be add because there is no source files for the target.")
+    else()
+        add_executable(
+            # The name of your game
+            ${name}
+            
+            ${source_files}
+        )
+    endif()
 
-    foreach (job_file IN LISTS jobs_files)
-        get_filename_component(job_name ${job_file} NAME_WLE)
+    if (TARGET uva-job)
+        file(GLOB_RECURSE jobs_files CONFIGURE_DEPENDS 
+            "${CMAKE_CURRENT_LIST_DIR}/src/jobs/*.cpp"
+        )
 
-        string(REPLACE "_" "-"   job_executable ${job_name})
+        foreach (job_file IN LISTS jobs_files)
+            get_filename_component(job_name ${job_file} NAME_WLE)
 
-        message(STATUS "Found job: ${job_name} (${job_executable})")
+            string(REPLACE "_" "-"   job_executable ${job_name})
 
-        add_executable(${job_executable} ${job_file})
-        target_link_libraries(${job_executable} uva-job uva-console uva-json)
-        target_compile_definitions(${job_executable} PUBLIC -D__UVA_JOB_COMPILATION__=1)
+            message(STATUS "Found job: ${job_name} (${job_executable})")
 
-        add_dependencies(${name} ${job_executable})
-    endforeach()
+            add_executable(${job_executable} ${job_file})
+            target_link_libraries(${job_executable} uva-job uva-console uva-json)
+            target_compile_definitions(${job_executable} PUBLIC -D__UVA_JOB_COMPILATION__=1)
+
+            add_dependencies(${name} ${job_executable})
+        endforeach()
+
+        target_link_libraries(${name} uva-job)
+    else()
+        message(STATUS "Job will be ignored because uva-job is not available")
+    endif()
+
+    if (TARGET uva-database)
+        target_link_libraries(${name} uva-database)
+    endif()
     
     target_compile_definitions(${name} PUBLIC -DAPP_ROOT="${CMAKE_CURRENT_LIST_DIR}")
 
@@ -58,6 +72,6 @@ function(add_web_app name)
         target_link_options(${name} PUBLIC /SAFESEH:NO)
     endif()
 
-    target_link_libraries(${name} uva-json uva-networking uva-job)
+    target_link_libraries(${name} uva-json uva-networking)
     
 endfunction(add_web_app)
